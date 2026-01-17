fcb78ee1f7440fcbbf66daecf1b630dee0a4a9ff
Martin <mr.mmaurer@gmail.com>
2026-01-17 19:22:54 +0100

    Added React compiler debugging

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import { devtools } from '@tanstack/devtools-vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
+import type { Logger } from 'babel-plugin-react-compiler';
 
 const config = defineConfig({
   plugins: [
@@ -15,10 +16,34 @@ const config = defineConfig({
     tanstackStart(),
     viteReact({
       babel: {
-        plugins: ['babel-plugin-react-compiler'],
+        plugins: [
+          [
+            'babel-plugin-react-compiler',
+            {
+              debug: true,
+              logger: {
+                logEvent(filename, event) {
+                  switch (event.kind) {
+                    case 'CompileSuccess': {
+                      console.log(`✅ Compiled: ${filename}`);
+                      break;
+                    }
+                    case 'CompileError': {
+                      console.log(`❌ Compiler Error: ${filename}`);
+                      console.error(`Reason: ${event.detail.reason}`);
+                      break;
+                    }
+                    default: {
+                    }
+                  }
+                },
+              } satisfies Logger,
+            },
+          ],
+        ],
       },
     }),
   ],
-})
+});
 
-export default config
+export default config;
